1d532fc137332ecaf4fa8ade6a9fff2bfcb21793
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-13 12:17:23 +0000

    changed interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,10 +8,18 @@ interface Teacher {
 }
 
 
-interface Directors {
+interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-interface Directors extends Teacher {
 
-}
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
