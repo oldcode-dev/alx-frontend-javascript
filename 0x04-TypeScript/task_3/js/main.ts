27173785f87db23c16872af28794bb1d405692a4
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-14 18:20:48 +0000

    refactor: changed updatedRow to use ...row instead of redeclaring user names

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -6,7 +6,7 @@ import * as CRUD from "./crud";
 const row: RowElement = { firstName: "Guillaume", lastName: "Salva" };
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 
 CRUD.deleteRow(newRowID);
