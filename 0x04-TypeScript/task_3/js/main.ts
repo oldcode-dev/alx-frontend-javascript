b55592921a8bc82be253c6bcc8378752b1ffa549
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-14 18:16:08 +0000

    refactor: changed obj varaible to row

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,8 +3,8 @@
 import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
-const obj: RowElement = { firstName: "Guillaume", lastName: "Salva" };
-const newRowID: RowID = CRUD.insertRow(obj);
+const row: RowElement = { firstName: "Guillaume", lastName: "Salva" };
+const newRowID: RowID = CRUD.insertRow(row);
 
 const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
